8a8d8ba6fe0c6c907932b6eb79c166326b50be77
hrishi2710 <hrishirules2710@gmail.com>
2020-10-10 14:54:23 +0530

    New build

---
+++ build/precache-manifest.c27621e7e4d1c7977c70255b0568a37f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1503ab9469048ddd37cc3804e135c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb986309d6ad93b77fc6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d6add9aa3ad03bbd747c",
+    "url": "/static/js/2.ff9e33cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff9e33cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb986309d6ad93b77fc6",
+    "url": "/static/js/main.f1dacf7d.chunk.js"
+  },
+  {
+    "revision": "e3316e4438bac8d51253",
+    "url": "/static/js/runtime-main.21d199d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
